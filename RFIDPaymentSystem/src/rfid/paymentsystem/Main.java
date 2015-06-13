e97d57832f7b7b1d1809bd8f894d69d0dbfa4741
loaded02 <ocaptainmycaptain@web.de>
2015-06-13 20:38:39 +0200

    bugfix

--- RFIDPaymentSystem/src/rfid/paymentsystem/Main.java
+++ RFIDPaymentSystem/src/rfid/paymentsystem/Main.java
@@ -6,8 +6,8 @@ import rfid.paymentsystem.controller.MainController;
 //Lukas Tag ID = lukasTagId
 //Moritz Tag ID = moritzTagId
 //Paul Tag ID = paulTagId 
-//10 Tag ID = 1
-//15 Tag ID = 2
+//10 Tag ID = valuetagid1
+//15 Tag ID = valuetagid2
 
 public class Main {
 	public static void main(String[] args) {
